d786aac7f616658e1d269546944f2a70dbf941d1
Your Name <new.anaspakattippara@gmail.com>
2023-12-05 20:18:32 +0530

    ass

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const person = {
     Name: "Muhammad Hanif",
     designation: "Business Consultant",
     des_link_text: "@ Naufal typing  ",
-    des_link: "https://www.google.com/maps/place/25%C2%B016'41.7%22N+55%C2%B018'45.7%22E/@25.2782383,55.3101044,17z/data=!3m1!4b1!4m4!3m3!8m2!3d25.2782383!4d55.3126793?hl=en&entry=ttu",
+    des_link: "https://maps.app.goo.gl/VAFpK6c9GWXZ1DKs6",
     contact: "./contact/Hanif.vcf",
 };
 
 const contact ={
     contact_no: "+97145487687",
     whatsapp: "+971562108012",
-    location: "",
+    location: "https://maps.app.goo.gl/VjCtQbDWKDZjTSt46",
     email: "Naufaltyping@gmail.com",
 };
 
